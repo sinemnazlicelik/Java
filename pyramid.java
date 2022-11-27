d219870371eaa6b16960ab6236238499a9fcd6b7
sinemnazlicelik <110194086+sinemnazlicelik@users.noreply.github.com>
2022-11-26 22:01:23 -0500

    Create pyramid.java

---
+++ pyramid.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+  for(int i=5; i>0; i--){
+      for(int j=6; j>i; j--){
+          System.out.print("*");
+      }
+      System.out.println();
+  }
+
+    }
+}
