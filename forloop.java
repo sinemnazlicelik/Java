d1051ce16eddea62095a60937cfbc826a9fbd2fc
sinemnazlicelik <110194086+sinemnazlicelik@users.noreply.github.com>
2022-11-26 21:52:30 -0500

    Create forloop.java

---
+++ forloop.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+  for(int k=0; k<5; k++){
+      for(int m=4; m>=k; m--){
+          System.out.print("*");
+      }
+      System.out.println();
+  }
+
+    }
+}
