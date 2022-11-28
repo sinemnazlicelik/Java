f4da851da6cc4c5248a91b5f9d914396fb6ace7a
sinemnazlicelik <110194086+sinemnazlicelik@users.noreply.github.com>
2022-11-28 06:54:48 +0300

    Add files via upload

---
+++ Projects - Beginner/Grocery Price Calculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+ double tomatoPrice = 2.50;
+ double cucumberPrice = 1.0;
+ double zucchiniPrice = 1.99;
+
+ System.out.println("How many kilos of tomatoes do you want?");
+    Double TomatoesKg = scan.nextDouble();
+ System.out.println("How many cucumber do you want?");
+    Double CucumberQuantity = scan.nextDouble();
+ System.out.println("How many kilos of zucchini do you want?");
+    Double zucchiniKg = scan.nextDouble();
+    Double TotalPrice =0.0;
+    TotalPrice=(tomatoPrice*TomatoesKg)+(cucumberPrice*CucumberQuantity)+(zucchiniPrice*zucchiniKg);
+    System.out.println("Total price of your shopping = "+TotalPrice+" $");
+
+    }
+}
